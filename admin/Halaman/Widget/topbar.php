3c89af3ea22425200b499cd0197a00662fe7627b
abdlhli <abdullahali.and1@gmail.com>
2022-11-24 23:06:40 +0700

    menambahkan fungsi sidebar

--- admin/Halaman/Widget/topbar.php
+++ admin/Halaman/Widget/topbar.php
@@ -1,13 +1,14 @@
 <body>
     <nav class="navbar navbar-expand navbar-theme">
         <a class="sidebar-toogle d-flex me-2">
-            <i class="hamburger align-self-center">
+            <i class="hamburger">
             </i>
         </a>
         <div class="navbar-collapse collapse">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown ms-lg-2">
-                    <a class="nav-link dropdown-toogle posisiton-relative" href="#" id="alert" data-bs-toogle="dropdown">
+                    <a class="nav-link dropdown-toogle posisiton-relative" href="#" id="alert"
+                        data-bs-toogle="dropdown">
                         <i class="align-middle fas fa-bell"></i>
                         <span class="indicator"></span>
                     </a>
--- admin/Halaman/index.php
+++ admin/Halaman/index.php
@@ -1,8 +1,3 @@
-<?php
-include_once('Komponen/header.php');
-include_once('Widget/Sidebar.php');
-?>
-
 <html>
 
 <?php
